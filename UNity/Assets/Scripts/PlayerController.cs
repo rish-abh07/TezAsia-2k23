16272a89a8ada3aa2b8404ec5bd24ec9f33cd748
Rishabh Pal <rish.09.rp@gmail.com>
2023-07-31 14:16:46 +0530

    map system updated

--- UNity/Assets/Scripts/PlayerController.cs
+++ UNity/Assets/Scripts/PlayerController.cs
@@ -9,6 +9,7 @@ public class PlayerController : MonoBehaviour
 
     float speed = 12f;
     public float gravity = -9.81f;
+    public float jumpHeight = 3.0f;
     // Start is called before the first frame update
     Vector3 velocity;
     bool isGrounded;
@@ -35,6 +36,11 @@ public class PlayerController : MonoBehaviour
         Vector3 move = transform.right * x + transform.forward * z;
 
         controller.Move(move * speed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
 
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
